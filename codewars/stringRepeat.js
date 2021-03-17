717b4ea9427d4acfa75c27fae142f871fad689b4
Linas Kriščiukaitis <l.krisciukaitis@users.noreply.github.com>
2021-03-17 16:33:00 +0200

    codewars

---
+++ codewars/stringRepeat.js
@@ -0,0 +1,18 @@
+function repeatStr (n, s) {
+    
+        for (let i = 0; i < array.length; i++) {
+            const element = array[i];
+            
+        }
+        
+    }
+
+    return ''
+}
+
+
+
+
+console.log(repeatStr(3, "*"), '->', "***");
+console.log(repeatStr(5, "#"), '->', "#####");
+console.log(repeatStr(2, "ha "), '->', "ha ha ");
--- codewars/sumStr.js
+++ codewars/sumStr.js
@@ -1,4 +1,20 @@
 function sumStr(a,b) {
+    const pirmas = parseInt(a);
+    const antras = parseInt(b);
 
+    if (isNaN(pirmas)) {
+        pirmas = 0;
+    }
+    if (isNaN(antras)) {
+        antras = 0;
+    }
 
+    return '' + (pirmas + antras);
 }
+
+
+console.log(sumStr("4","5"), '->', "9");
+console.log(sumStr("34","5"), '->', "39");
+console.log(sumStr("","5"), '->', "5");
+console.log(sumStr("34",""), '->', "34");
+console.log(sumStr("",""), '->', "0");
