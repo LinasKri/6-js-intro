a3428509bdc5835426fad49c30ece59c59a8412f
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-03-23 00:51:51 +0200

    hopper in progress

---
+++ codewars/grasshopperSummation.js
@@ -0,0 +1,15 @@
+/*
+https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/javascript
+*/
+
+var summation = function (num) {
+    for (let i = num; i > 0; i--) {
+        
+        return num + num--
+    }
+}
+
+
+
+console.log(summation(1), 1)
+console.log(summation(8), 36)
