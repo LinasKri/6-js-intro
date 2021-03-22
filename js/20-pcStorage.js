e30ca35c05b9c87c78a240fb7b2eff83b029c0b0
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-03-22 12:19:38 +0200

    PC Storage pratimas

--- js/20-pcStorage.js
+++ js/20-pcStorage.js
@@ -3,8 +3,24 @@ Duota kietojo disko duomenu struktura (folder > file) ir reikia suskaiciuoti, ki
 Folder uzima 0 vietos.
 */
 
-function storage() {
-    return 0;
+function storage(folder) {
+    let totalSize = 0;
+   // console.log(folder);
+
+    for (let i = 0; i < folder.content.length; i++) {
+        const child = folder.content[i];
+      //  console.log(child);
+
+        if (child.type === 'file') {
+            totalSize += child.size
+
+        } else if (child.type === 'folder') {
+            totalSize += storage(child)
+        }
+        
+    }
+
+    return totalSize;
 }
 
 const pc = {
