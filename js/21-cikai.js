807282be2b6e741673b8e664616e68cfa99b4c6a
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-03-23 13:35:52 +0200

    Ciklai

--- js/21-cikai.js
+++ js/21-cikai.js
@@ -85,6 +85,7 @@ console.clear()
 
 
 // DO-WHILE    pirmiausia daro, paskui klausia
+
 //break: nutraukia ciklo darba
 
 let dwi = 0;
@@ -98,3 +99,133 @@ do {
 } while (dwi < 5);
 
 console.clear()
+
+// FOREACH
+
+// tai yra cikas, kuris automatiskai eina per sarasa;
+// nuo pirmo iki paskutinio nario;
+
+let suma = 0
+
+list.forEach(function (item, i, /*arr*/) {   
+    console.log(i, item, /*arr*/);
+})
+
+/* list.forEach(item, i, arr) => {   
+    console.log(i, item, arr);
+}) */
+
+/* list.forEach(item => console.log(item);
+}) */
+let FEsuma = 0;
+list.forEach (n => FEsuma += n)
+console.log(FEsuma);
+
+console.clear()
+
+const doubleList = [[1, 5], 15, [8, 99], -40, [-8, -7, 15, 4], [3]];
+let dlSuma = 0;
+// doubleList.forEach(innerItem => innerItem.forEach(n => dlSuma += n))
+doubleList.forEach(
+    innerItem => typeof innerItem === 'number'
+    ? dlSuma += innerItem
+    :innerItem.forEach(n => dlSuma+= n)
+);
+console.log(dlSuma);
+
+console.clear()
+
+// MAP
+// map() metodas perejes per duota sarasa grazina tokio pacio dydzop sarasa,
+//tik su pakeistomis reiksmemis
+
+// tekstams MAP neveikia, tik array
+
+const mapList = [1, 2, 3, 4, 5, 6];
+const forListSqaure = [];
+
+for (const num of mapList) {
+    forListSqaure.push(num * num);
+}
+console.log(forListSqaure);
+
+const mapListSquare = mapList.map(n => n * n);
+console.log(mapListSquare);
+
+/**********************************************************/
+console.clear()
+
+// uzdavinys: visi neigiami skaiciai turi buti konvertuoti i nuli
+const pazymiai = [10, -8, 2, -6, 8, -2];
+
+const konvertuotiPazymiai = pazymiai.map(n => n < 0 ? 0 : n);
+console.log(pazymiai);
+console.log(konvertuotiPazymiai);
+
+/**********************************************************/
+console.clear()
+
+const desimtainiai = [3.14, 5, 6.7, -14.7, -8, 0.001];
+
+const sveikieji = desimtainiai.map(n => Math.round(n));
+console.log(desimtainiai);
+console.log(sveikieji);
+
+/**********************************************************/
+console.clear()
+// pvz su FOR, bet problema, kad reikia overdide'inti const
+for (let i = 0; i < desimtainiai.length; i++) {
+    desimtainiai[i] = Math.round(desimtainiai[i]);
+}
+console.log(desimtainiai);    
+
+/**********************************************************/
+console.clear()
+
+// FILTER        skirtas modifikuoti reiksmes
+
+// atrinkti tik tikrus pazymius,t.y. [1..10]
+
+const marks = [10, -8, 2, 3.14, -6, 7.5, 8, -2, 11, 55];
+
+const filteredMarks = marks.filter(m => m > 0 && m <= 10 && m % 1 === 0);
+
+console.log(marks);
+console.log(filteredMarks);
+
+// palikti zodius is didziosios raides
+
+const dictionary = ['Labas', 'rytas', 'Lietuva', 'sakau', 'tau'];
+
+const isDidziosios = dictionary.filter(word => word[0] === word[0].toUpperCase());
+
+console.log(dictionary);
+console.log(isDidziosios);
+
+
+/**********************************************************/
+console.clear()
+
+// REDUCE
+// eina per sarasa, paima visas reiksmes is eiles ir su jomis padaro viena bendra rezultata
+// pvz .: jei turim pazymiu sarasa, tai eina per pazymius ir suskaiciuoja pzymiu suma
+
+
+const pertiukoPazymiai = [10, 2, 8, 6, 4];
+
+let pertiukoPazymiuSuma = 0;
+pertiukoPazymiai.forEach(p => pertiukoPazymiuSuma += p);
+console.log(pertiukoPazymiuSuma, 'nes petriukoPazymiai 0');
+
+const reducedPazymiai = pertiukoPazymiai.reduce((total, paz) => total + paz, 20);
+console.log(reducedPazymiai, 'butu 30 bet yra prideta 20');
+
+
+/**********************************************************/
+console.clear()
+
+// koks gausis tekstas, jei sujungsime visu zoddziu pirmas raides
+const dictionary2 = ['Labas', 'rytas', 'Lietuva', 'sakau', 'tau'];
+
+const short = dictionary2.reduce((total, word) => total + word[0], '');
+console.log(short);
