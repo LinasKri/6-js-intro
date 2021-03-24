0faa06881eb7c341374cc570b4aa33d7a94359ab
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-03-24 15:09:34 +0200

    grasshopper

--- codewars/grasshopperSummation.js
+++ codewars/grasshopperSummation.js
@@ -3,10 +3,11 @@ https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/javascript
 */
 
 var summation = function (num) {
-    for (let i = num; i > 0; i--) {
-        
-        return num + num--
+    let sum = 0
+    for(let i = 0; i <= num; i++) {
+        sum += i
     }
+    return sum
 }
 
 
