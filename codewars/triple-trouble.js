525bdbe3f4b7d882053f566a83f5cb2f2b664a5b
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-03-25 09:42:03 +0200

    triple trouble

---
+++ codewars/triple-trouble.js
@@ -0,0 +1,15 @@
+/*
+https://www.codewars.com/kata/5704aea738428f4d30000914/train/javascript
+*/
+
+function tripleTrouble(one, two, three){
+    
+    
+
+    }
+
+
+console.log(tripleTrouble("this","test","lock"), "ttlheoiscstk");
+console.log(tripleTrouble("aa","bb","cc"), "abcabc");
+console.log(tripleTrouble("Bm", "aa", "tn"), "Batman");
+console.log(tripleTrouble("LLh","euo","xtr"), "LexLuthor");
