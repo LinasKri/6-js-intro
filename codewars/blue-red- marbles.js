6522e07c336baf5a5826db4ae5ba6eb7bced5ed7
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-03-28 23:00:37 +0300

    blue red

--- codewars/blue-red- marbles.js
+++ codewars/blue-red- marbles.js
@@ -11,5 +11,6 @@ function guessBlue(blueStart, redStart, bluePulled, redPulled) {
 console.log('-------------------------');
 
 console.log(guessBlue(5, 5, 2, 3), 0.6);
+
 console.log(guessBlue(5, 7, 4, 3), 0.2);
 console.log(guessBlue(12, 18, 4, 6), 0.4);
